3af8ad94763eca362c007976ec8c0d95b9c6cb6e
mayardes <mayardesoliveira@gmail.com>
2022-02-07 11:36:15 -0400

    feat: install packages, modeling classes on DbSet and configuring connection string - install packages: Microsoft.EntityFrameworkCore and EntityFrameworkCoreSqlServer - modeling classes: Category, Role, Post, Tag, User and UserRole - configuring: configuring connection string overriding on method OnConfiguring in BlogDataContext

---
+++ ConcepsEFCore/Blog/Data/BlogDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-M3HIHDH\SQLEXPRESS; Database=BlogDatabase; Integrated Secutiry=true;");
+        }
+    }
+}
---
+++ ConcepsEFCore/Blog/Models/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Models
+{
+    public class UserRole
+    {
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+    }
+}
